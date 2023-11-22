66f14efe7ddd517091007a08c69269a24720a98d
Wil-alf <wilson.535220219@stu.untar.ac.id>
2023-11-22 13:07:33 +0700

    second commit

---
+++ lib/sceens/detail_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:testflutter/provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:testflutter/sceens/main_screen.dart';
+
+class DetailScreen extends ConsumerStatefulWidget {
+  const DetailScreen({super.key});
+
+  static String routeName = '/detail_screen';
+
+  @override
+  ConsumerState<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends ConsumerState<DetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final counterProv = ref.watch(counterProvider);
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Detail'),
+        leading: IconButton(
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context)=>MyHomePage()));
+              setState(() {
+                
+              });
+            },
+            icon: Icon(Icons.arrow_back),
+          ),
+        actions: <Widget>[
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button in the previous screen this many times:',
+            ),
+            Text(
+              counterProv.getCounter().toString(),
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            const SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: () {
+                 setState(() {
+                   counterProv.resetCounter();
+                 });
+                
+              },
+              child: const Text('Clear Data'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
